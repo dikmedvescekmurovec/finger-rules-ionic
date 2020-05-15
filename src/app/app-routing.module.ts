14b3539733c3feacf304e36e045c8b0d06dd0ddb
Dik Medvešček Murovec <dm3825@student.uni-lj.si>
2020-05-15 04:41:49 +0200

    Updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'meeting/:meetingID',
     loadChildren: () => import('./meeting/meeting.module').then( m => m.MeetingPageModule)
   },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
 
 @NgModule({
