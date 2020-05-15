bff70c889c2fea21275ef91fb8b6a2d1e25f8f91
Dik Medvešček Murovec <dm3825@student.uni-lj.si>
2020-05-15 04:42:48 +0200

    Updated routing again. Leave empty route for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'start',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
@@ -12,7 +12,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'start'
   }
 ];
 
