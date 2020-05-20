f425bdf7d2e18e873ed01138ffbe801907c8d6dc
Žan Ožbot <zan.ozbot@gmail.com>
2020-05-20 19:20:16 +0200

    Switched clipboard with angular implementation

--- src/app/meeting/meeting.module.ts
+++ src/app/meeting/meeting.module.ts
@@ -1,9 +1,7 @@
-import { ClipboardModule } from '@angular/cdk/clipboard';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
-import { Clipboard } from '@ionic-native/clipboard/ngx';
 import { IonicModule } from '@ionic/angular';
 import { KebabCasePipe } from '../kebab-case.pipe';
 import { MeetingPageRoutingModule } from './meeting-routing.module';
@@ -11,6 +9,7 @@ import { MeetingPage } from './meeting.page';
 import { EmptyRuleListComponent } from './rule-list/empty-rule-list/empty-rule-list.component';
 import { FingerRuleComponent } from './rule-list/finger-rule/finger-rule.component';
 import { RuleListComponent } from './rule-list/rule-list.component';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
   imports: [
@@ -26,8 +25,6 @@ import { RuleListComponent } from './rule-list/rule-list.component';
     FingerRuleComponent,
     KebabCasePipe,
     EmptyRuleListComponent],
-  providers: [
-    Clipboard
-  ]
+  providers: []
 })
 export class MeetingPageModule { }
